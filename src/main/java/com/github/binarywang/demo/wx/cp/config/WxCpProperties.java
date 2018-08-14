58d34ed630e9c8931a8bf316ba92640e9caa0d2a
Vincent.Chan <vencent@gmail.com>
2018-08-14 20:19:56 +0800

    test

--- src/main/java/com/github/binarywang/demo/wx/cp/config/WxCpProperties.java
+++ src/main/java/com/github/binarywang/demo/wx/cp/config/WxCpProperties.java
@@ -1,11 +1,10 @@
 package com.github.binarywang.demo.wx.cp.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * * @author Binary Wang(https://github.com/binarywang)
@@ -14,33 +13,33 @@ import lombok.Setter;
 @Setter
 @ConfigurationProperties(prefix = "wechat.cp")
 public class WxCpProperties {
-  /**
-   * 设置微信企业号的corpId
-   */
-  private String corpId;
+    /**
+     * 设置微信企业号的corpId
+     */
+    private String corpId;
 
-  /**
-   * 设置微信企业应用的AgentId
-   */
-  private Integer agentId;
+    /**
+     * 设置微信企业应用的AgentId
+     */
+    private Integer agentId;
 
-  /**
-   * 设置微信企业应用的Secret
-   */
-  private String secret;
+    /**
+     * 设置微信企业应用的Secret
+     */
+    private String secret;
 
-  /**
-   * 设置微信企业号的token
-   */
-  private String token;
+    /**
+     * 设置微信企业号的token
+     */
+    private String token;
 
-  /**
-   * 设置微信企业号的EncodingAESKey
-   */
-  private String aesKey;
+    /**
+     * 设置微信企业号的EncodingAESKey
+     */
+    private String aesKey;
 
-  @Override
-  public String toString() {
-    return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
-  }
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
 }
--- src/main/java/com/github/binarywang/demo/wx/cp/controller/WxErrorController.java
+++ src/main/java/com/github/binarywang/demo/wx/cp/controller/WxErrorController.java
@@ -1,8 +1,6 @@
 package com.github.binarywang.demo.wx.cp.controller;
 
-import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
@@ -15,7 +13,8 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * @author Binary Wang(https://github.com/binarywang)
@@ -23,80 +22,80 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class WxErrorController implements ErrorController {
-  private final static String ERROR_PATH = "/error";
-  private static WxErrorController appErrorController;
+    private final static String ERROR_PATH = "/error";
+    private static WxErrorController appErrorController;
 
-  /**
-   * Error Attributes in the Application
-   */
-  @Autowired
-  private ErrorAttributes errorAttributes;
+    /**
+     * Error Attributes in the Application
+     */
+    @Autowired
+    private ErrorAttributes errorAttributes;
 
-  /**
-   * Controller for the Error Controller
-   */
+    /**
+     * Controller for the Error Controller
+     */
 
-  public WxErrorController(ErrorAttributes errorAttributes) {
-    this.errorAttributes = errorAttributes;
-  }
-
-  public WxErrorController() {
-    if (appErrorController == null) {
-      appErrorController = new WxErrorController(this.errorAttributes);
-    }
-  }
-
-  /**
-   * Supports the HTML Error View
-   */
-  @RequestMapping(value = ERROR_PATH, produces = "text/html")
-  public ModelAndView errorHtml(HttpServletRequest request) {
-    return new ModelAndView("error", this.getErrorAttributes(request, false));
-  }
-
-  /**
-   * Supports other formats like JSON, XML
-   */
-  @RequestMapping(value = ERROR_PATH)
-  @ResponseBody
-  public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
-    Map<String, Object> body = this.getErrorAttributes(request, this.getTraceParameter(request));
-    HttpStatus status = this.getStatus(request);
-    return new ResponseEntity<>(body, status);
-  }
-
-  @Override
-  public String getErrorPath() {
-    return ERROR_PATH;
-  }
-
-  private boolean getTraceParameter(HttpServletRequest request) {
-    String parameter = request.getParameter("trace");
-    if (parameter == null) {
-      return false;
+    public WxErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
     }
 
-    return !"false".equals(parameter.toLowerCase());
-  }
-
-  private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
-    RequestAttributes requestAttributes = new ServletRequestAttributes(request);
-    Map<String, Object> map = this.errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
-    log.error("map is [{}]", map);
-    String url = request.getRequestURL().toString();
-    map.put("URL", url);
-    log.error("[error info]: status-{}, request url-{}", map.get("status"), url);
-    return map;
-  }
-
-  @SuppressWarnings("static-method")
-  private HttpStatus getStatus(HttpServletRequest request) {
-    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
-    if (statusCode != null) {
-      return HttpStatus.valueOf(statusCode);
+    public WxErrorController() {
+        if (appErrorController == null) {
+            appErrorController = new WxErrorController(this.errorAttributes);
+        }
     }
 
-    return HttpStatus.INTERNAL_SERVER_ERROR;
-  }
+    /**
+     * Supports the HTML Error View
+     */
+    @RequestMapping(value = ERROR_PATH, produces = "text/html")
+    public ModelAndView errorHtml(HttpServletRequest request) {
+        return new ModelAndView("error", this.getErrorAttributes(request, false));
+    }
+
+    /**
+     * Supports other formats like JSON, XML
+     */
+    @RequestMapping(value = ERROR_PATH)
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+        Map<String, Object> body = this.getErrorAttributes(request, this.getTraceParameter(request));
+        HttpStatus status = this.getStatus(request);
+        return new ResponseEntity<>(body, status);
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+
+    private boolean getTraceParameter(HttpServletRequest request) {
+        String parameter = request.getParameter("trace");
+        if (parameter == null) {
+            return false;
+        }
+
+        return !"false".equals(parameter.toLowerCase());
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        Map<String, Object> map = this.errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+        log.error("map is [{}]", map);
+        String url = request.getRequestURL().toString();
+        map.put("URL", url);
+        log.error("[error info]: status-{}, request url-{}", map.get("status"), url);
+        return map;
+    }
+
+    @SuppressWarnings("static-method")
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode != null) {
+            return HttpStatus.valueOf(statusCode);
+        }
+
+        return HttpStatus.INTERNAL_SERVER_ERROR;
+    }
 
 }
